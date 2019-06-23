aba500006a56775c6e164f99e2dba507e7fbcbf1
Oumar Djimé Ratou <oudjira90@gmail.com>
2019-06-23 11:54:56 +0100

    Metéo

---
+++ meteo/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name="meteo",
+    version="1.0.0",
+    description="A Python package to get weather reports for any location.",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/oumar90/meteo",
+    author="Oumar Djimé Ratou",
+    author_email="oudjira90@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["weather_reporter"],
+    include_package_data=True,
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "weather-reporter=weather_reporter.cli:main",
+        ]
+    },
+)
